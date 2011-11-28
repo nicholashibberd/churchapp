2171d5dea95ef8f7562860fffdea475eccb5737d
Nick Hibberd <nicholashibberd@hotmail.com>
2011-11-28 14:32:11 +0000

    removing raise

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -18,7 +18,7 @@ class DocumentsController < ApplicationController
   
   def show
     @document = Document.find(params[:id])
-    raise @document.pdf.path(:original).inspect
+    #raise @document.pdf.path(:original).inspect
     return send_file @document.pdf.path(:original), :type => "application/pdf", :filename => "making_space.pdf", :disposition => 'inline'
   end
   
