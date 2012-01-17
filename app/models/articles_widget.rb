bc8772c4fb4831195d94115294241d1745a0b4c2
nicholashibberd <nicholashibberd@gmail.com>
2012-01-17 13:53:45 +0000

    changing the default snippet to be an integer

--- app/models/articles_widget.rb
+++ app/models/articles_widget.rb
@@ -1,5 +1,5 @@
 class ArticlesWidget < Widget
   field :number_to_display
-  field :snippet_size, :default => 300
+  field :snippet_size, :type => Integer, :default => 300
   belongs_to :category
 end
